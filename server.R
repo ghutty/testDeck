9e296ed514a7759f86f2918be06fd40f1643b494
ghutty <patrick.guzman@gmail.com>
2014-06-23 08:28:09 -0700

    Commit

--- server.R
+++ server.R
@@ -1,16 +1,35 @@
 # server.R
 
+library(maps)
+counties <- readRDS("data/counties.rds")
+source("helpers.R")
+
 shinyServer(
   function(input, output) {
-    
-    output$text1 <- renderText({ 
-      paste("You have selected", input$var)
+    output$map <- renderPlot({
+      data <- switch(input$var, 
+                     "Percent White" = counties$white,
+                     "Percent Black" = counties$black,
+                     "Percent Hispanic" = counties$hispanic,
+                     "Percent Asian" = counties$asian)
+      
+      color <- switch(input$var, 
+                      "Percent White" = "darkgreen",
+                      "Percent Black" = "black",
+                      "Percent Hispanic" = "darkorange",
+                      "Percent Asian" = "darkviolet")
+      
+      legend <- switch(input$var, 
+                       "Percent White" = "% White",
+                       "Percent Black" = "% Black",
+                       "Percent Hispanic" = "% Hispanic",
+                       "Percent Asian" = "% Asian")
+      
+      percent_map(var = data, 
+                  color = color, 
+                  legend.title = legend, 
+                  max = input$range[2], 
+                  min = input$range[1])
     })
-    
-    output$text2 <- renderText({ 
-      paste("You have chosen a range that goes from",
-            input$range[1], "to", input$range[2])
-    })
-    
   }
 )
